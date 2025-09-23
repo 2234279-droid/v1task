b350bef4030a0d028d5c29ac4c5865311c033554
Aalain <2234279@upt.edu.mx>
2025-09-23 15:30:32 -0600

    v4.2

--- src/pages/tasks/new.js
+++ src/pages/tasks/new.js
@@ -2,14 +2,47 @@ import { Form, Grid, Button} from 'semantic-ui-react';
 import {useState} from 'react';
 
 export default function TaskFormPage() {
-  const [newTask, serNewTask] = useState({title: "", description:""});
+  const [newTask, setNewTask] = useState({title: "", description:  ""});
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("enviando formulario");
+  const [errors, setErrors] = useState({
+    title: "",
+    description: "",
+  });
+
+  const validate = () => {
+    const errors = {};
+
+    if (!newTask.title) errors.title = "Title is required";
+    if (!newTask.description) errors.description = "Description is requeired";
+
+    return errors;
   }
 
-  const handleChange = (e) => setNewTask({[e.target.name]: e.target.value});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let errors = validate();
+
+    if (Object.keys(errors).length) return setErrors(errors);
+
+    await createTask();
+  };
+
+
+    const createTask = async () => {
+      try {
+        await fetch("http://localhost:3000/api/tasks", {
+          method: "POST",
+          headers: {
+            "content-type": "aplication/json"
+          },
+          body: JSON.stringify(newTask)
+        })
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  const handleChange = (e) => setNewTask({...newTask, [e.target.name]: e.target.value});
 
   return (
     <Grid centered verticalAlign="middle" columns="3" style={{height: "80vh"}}>
@@ -22,12 +55,14 @@ export default function TaskFormPage() {
             placeholder="Title"
             name="title"
             onChange={handleChange}
+            error = {errors.title ? {content: errors.title, pointing: "below"} : null }
             />
             <Form.TextArea
             label="Description"
-            placeholder="Description
+            placeholder="Description"
             name="description"
             onChange={handleChange}
+            error = {errors.description ? {content: errors.description, pointing: "below"} : null }
             />
             <Button>Save</Button>
           </Form>
